db5fa00c4c9a9d1931b0d3117a45f8936e44b556
BrinzaBogdan <bogdan.brinza2020@gmail.com>
2025-09-05 20:04:10 +0300

    Finished Hero Section & Page Layout

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+
+import "./globals.css";
+import { Footer, Navbar } from "@/components";
+
+
+export const metadata: Metadata = {
+  title: "Create Next App",
+  description: "Discover the best cars in the world",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body className="relative">
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
+    </html>
+  );
+}
