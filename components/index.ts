0fc9a3e56726b8d5d2b4ccbd4ca40bd874c838f6
BrinzaBogdan <bogdan.brinza2020@gmail.com>
2025-09-05 21:25:50 +0300

    Added the 1+letter-word search functionality

--- components/index.ts
+++ components/index.ts
@@ -2,6 +2,9 @@ import Hero from './Hero'
 import CustomButton from './CustomButton';
 import Navbar from './Navbar';
 import Footer from './Footer';
+import SearchBar from './SearchBar';
+import CustomFilter from './CustomFilter';
+import SearchManufacturer from './SearchManufacturer';
 
 
 
@@ -10,5 +13,8 @@ export {
   CustomButton,
   Navbar,
   Footer,
+  SearchBar,
+  CustomFilter,
+  SearchManufacturer,
 }
 
